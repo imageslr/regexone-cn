fc77875e7e3050f5a800a966904f21ffb32cd348
elonz <zhukaihao.r@bytedance.com>
2021-06-03 14:05:17 +0800

    add: create all files
    Change-Id: Id050e92f618aa9eff9eaac67b256c07dc46b43aa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,11 +15,14 @@ module.exports = {
     editLinkText: "编辑此页面",
 
     nav: [
+      { text: '课程', link: '/lesson/introduction_and_abcs' },
+      { text: '问题', link: '/problem/matching_decimal_numbers' },
       { text: 'RegexOne 官方网站', link: 'https://regexone.com' },
     ],
 
     sidebar: [{
         title: "课程",
+        // collapsable: false,
         children: [
           "/lesson/introduction_and_abcs",
           "/lesson/letters_and_digits",
@@ -27,7 +30,31 @@ module.exports = {
           "/lesson/matching_characters",
           "/lesson/excluding_characters",
           "/lesson/character_ranges",
-          "/lesson/repeating_characters"
+          "/lesson/repeating_characters",
+          "/lesson/kleene_operators",
+          "/lesson/optional_characters",
+          "/lesson/whitespaces",
+          "/lesson/line_beginning_end",
+          "/lesson/capturing_groups",
+          "/lesson/nested_groups",
+          "/lesson/more_groups",
+          "/lesson/conditionals",
+          "/lesson/misc_meta_characters",
+          "/lesson/end"
+        ]
+      }, {
+        title: "问题",
+        // collapsable: false,
+        children: [
+          "/problem/matching_decimal_numbers",
+          "/problem/matching_phone_numbers",
+          "/problem/matching_emails",
+          "/problem/matching_html",
+          "/problem/matching_filenames",
+          "/problem/trimming_whitespace",
+          "/problem/extracting_log_data",
+          "/problem/extracting_url_data",
+          "/problem/complete"
         ]
       }]
   },
